b227aafbb14812b2076edc00aa59077e4c966a89
Romiono <roma.serebrennikov.00@mail.ru>
2025-06-26 04:03:43 +0300

    add request page and mock api by jsonplaceholder

---
+++ src/api/UserApi.ts
@@ -0,0 +1,36 @@
+interface Geo {
+    lat: string;
+    lng: string;
+}
+
+interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+}
+
+interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
+}
+
+export class UserApi {
+    static async get(): Promise<User[]> {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        return await res.json()
+    }
+}
--- src/pages/request/request.tsx
+++ src/pages/request/request.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import { type User, UserApi } from "../../api/UserApi.ts";
 
 const Request = () => {
+
+    const [user, setUser] = useState<User[]>([]);
+    useEffect( () => {
+        getUsers().then(res => console.log(res));
+    }, []);
+
+    const getUsers = async () => {
+        try {
+            const res = await UserApi.get()
+            setUser(res)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    if (!user) return <div>Нет данных</div>
+
     return (
         <div>
-
+            {user.map((item) => (
+                <div>
+                    <h3>{item.name}</h3>
+                    <p>{item.email}</p>
+                </div>
+            ))}
         </div>
     );
 };
